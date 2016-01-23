661ccba57ef45d40939b4815c887de2a0b178262
Kurt McAlpine <kurt@linux.com>
2016-01-23 16:44:19 +1300

    Code style and refactoring

--- src/main.c
+++ src/main.c
@@ -3,29 +3,29 @@
 #include <string.h>
 #include "config_handler.h"
 
-#define CTRLD 	4
+#define CTRLD 4
 
 ITEM **my_items;
-int c;				
+int c;
 MENU *my_menu;
 int n_choices, i;
 ITEM *cur_item;
-int size = 0;
-struct boot_option **boot_options = NULL;
+int size;
+struct boot_option **boot_options;
 
-void build_menu()
+void build_menu(void)
 {
 	clear();
 	/* Initialize items */
 	n_choices = size;
 	my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 	char *str;
-	for(i = 0; i < n_choices; ++i) {
-		if (boot_options[i]->root) {
+
+	for (i = 0; i < n_choices; ++i) {
+		if (boot_options[i]->root)
 			str = boot_options[i]->root;
-		} else {
+		else
 			str = "";
-		}
 		my_items[i] = new_item(boot_options[i]->menu_label, str);
 	}
 	my_items[n_choices] = (ITEM *)NULL;
@@ -40,22 +40,27 @@ void build_menu()
 	set_menu_format(my_menu, 40, 1);
 
 	/* Post the menu */
-	mvprintw(LINES - 4, 0, "Hightlight option and press 'd' to select it for deletion");
-	mvprintw(LINES - 3, 0, "J and K arrow keys to naviage, q to quit");
-	mvprintw(LINES - 2, 0, "After selecting options press a to apply changes");
+	mvprintw(LINES - 4, 0,
+			"Hightlight option and press 'd' to select it for deletion");
+	mvprintw(LINES - 3, 0,
+			"J and K arrow keys to naviage, q to quit");
+	mvprintw(LINES - 2, 0,
+			"After selecting options press a to apply changes");
 	post_menu(my_menu);
 }
 
 int main(void)
 {
 	char *config_file = NULL;
+
 	add_to_string(&config_file, BOOT_DIR);
 	add_to_string(&config_file, "/syslinux/syslinux.cfg");
 	int line_number = 0;
-	parse_config_file(&boot_options, &size, &line_number);
-	
 
-	/* Initialize curses */	
+	parse_config_file(&boot_options, &size, &line_number);
+
+
+	/* Initialize curses */
 	initscr();
 	start_color();
 	cbreak();
@@ -71,66 +76,74 @@ int main(void)
 	int *indexes;
 	int num_indexes;
 
-	while((c = getch()) != KEY_F(1))
-	{       switch(c)
-		{	case KEY_DOWN:
-				menu_driver(my_menu, REQ_DOWN_ITEM);
-				break;
-			case 'j':
-				menu_driver(my_menu, REQ_DOWN_ITEM);
-				break;
-			case KEY_UP:
-				menu_driver(my_menu, REQ_UP_ITEM);
-				break;
-			case 'k':
-				menu_driver(my_menu, REQ_UP_ITEM);
-				break;
-			case 10: /* Enter */
-				move(20, 0);
-				clrtoeol();
-				const char *selected = item_name(current_item(my_menu));
-				if (strcmp(selected, "Exit") == 0) {
-					goto exit;
-				}
-				mvprintw(20, 0, "Item selected is : %s", selected);
-				pos_menu_cursor(my_menu);
-				break;
-			case 'd':
-				if ((O_SELECTABLE & item_opts(current_item(my_menu))) == O_SELECTABLE) {
-					item_opts_off(current_item(my_menu), O_SELECTABLE);
-				} else {
-					item_opts_on(current_item(my_menu), O_SELECTABLE);
-				}
-				break;
-			case 'a':
-				indexes = NULL;
-				num_indexes = 0;
-				for (int i = 0; i < n_choices; i++) {
-					if ((O_SELECTABLE & item_opts(my_items[i])) != O_SELECTABLE) {
-						indexes = realloc(indexes, ++num_indexes * sizeof(int));
-						indexes[num_indexes - 1] = i;
+	while ((c = getch()) != KEY_F(1)) {
+		switch (c) {
+		case KEY_DOWN:
+			menu_driver(my_menu, REQ_DOWN_ITEM);
+			break;
+		case 'j':
+			menu_driver(my_menu, REQ_DOWN_ITEM);
+			break;
+		case KEY_UP:
+			menu_driver(my_menu, REQ_UP_ITEM);
+			break;
+		case 'k':
+			menu_driver(my_menu, REQ_UP_ITEM);
+			break;
+		case 10: /* Enter */
+			move(20, 0);
+			clrtoeol();
+			const char *selected = item_name(current_item(my_menu));
 
-
-					}
-				}
-				for (int i = num_indexes - 1; i >= 0; i--) {
-					delete_configuration(&boot_options, &size, indexes[i]);
-				}
-				build_menu();
-				refresh();
-				output_config_file(boot_options, size, line_number, "test.txt", config_file);
-
-				break;
-				
-
-			case 'q':
+			if (strcmp(selected, "Exit") == 0)
 				goto exit;
+			mvprintw(20, 0, "Item selected is : %s", selected);
+			pos_menu_cursor(my_menu);
+			break;
+		case 'd':
+			if ((O_SELECTABLE & item_opts(current_item(my_menu)))
+					== O_SELECTABLE) {
+				item_opts_off(current_item(my_menu),
+						O_SELECTABLE);
+			} else {
+				item_opts_on(current_item(my_menu),
+						O_SELECTABLE);
+			}
+			break;
+		case 'a':
+			indexes = NULL;
+			num_indexes = 0;
+			for (int i = 0; i < n_choices; i++) {
+				if ((O_SELECTABLE & item_opts(my_items[i])) !=
+						O_SELECTABLE) {
+					indexes = realloc(indexes,
+							++num_indexes *
+							sizeof(int));
+					indexes[num_indexes - 1] = i;
+
+
+				}
+			}
+			for (int i = num_indexes - 1; i >= 0; i--) {
+				delete_configuration(&boot_options, &size,
+						indexes[i]);
+			}
+			build_menu();
+			refresh();
+			output_config_file(boot_options, size, line_number,
+					"test.txt", config_file);
+
+			break;
+
+
+		case 'q':
+			goto exit;
 		}
-	}	
+	}
 
 exit:
 	unpost_menu(my_menu);
-	for(i = 0; i < n_choices; ++i)
+	for (i = 0; i < n_choices; ++i)
 		free_item(my_items[i]);
 	free_menu(my_menu);
 	endwin();
